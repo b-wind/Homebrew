a27626131132f670151ab49488564e5198a5d1e8
b-wind <perl.mania@gmail.com>
2019-11-09 18:57:22 +0900

    add font-ricty-diminished.rb

---
+++ Casks/font-ricty-diminished.rb
@@ -0,0 +1,17 @@
+cask 'font-ricty-diminished' do
+  version '4.1.1'
+  sha256 '09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b'
+
+  url "https://www.rs.tus.ac.jp/yyusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  name 'Ricty Diminished'
+  homepage 'https://www.rs.tus.ac.jp/yyusa/ricty_diminished.html'
+
+  font 'RictyDiminished-Bold.ttf'
+  font 'RictyDiminished-BoldOblique.ttf'
+  font 'RictyDiminished-Oblique.ttf'
+  font 'RictyDiminished-Regular.ttf'
+  font 'RictyDiminishedDiscord-Bold.ttf'
+  font 'RictyDiminishedDiscord-BoldOblique.ttf'
+  font 'RictyDiminishedDiscord-Oblique.ttf'
+  font 'RictyDiminishedDiscord-Regular.ttf'
+end
