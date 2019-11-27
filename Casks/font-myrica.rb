22cb960c2b7ff22a0fdda612cc5623b302a5ed0a
b-wind <perl.mania@gmail.com>
2019-11-28 02:57:23 +0900

    update Download urls.

--- Casks/font-myrica.rb
+++ Casks/font-myrica.rb
@@ -2,7 +2,8 @@ cask 'font-myrica' do
   version '2.012.20180119'
   sha256 '189698851150814728a093830dd61b685dd98a01f29e36738d9271420726202b'
 
-  url "https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip"
+  ## url "https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip"
+  uurl "https://github.com/tomokuni/Myrica/blob/a2383726d3662453ef7efa37862f743a831c7433/product/Myrica.zip?raw=true"
   name 'Myrica'
   homepage 'https://myrica.estable.jp/'
 
--- Casks/font-myricam.rb
+++ Casks/font-myricam.rb
@@ -2,7 +2,8 @@ cask 'font-myricam' do
   version '2.012.20180119'
   sha256 'd1bb313ba9d3d22c7a084369ef9463c75735d3165a223c66596cd60f285379a6'
 
-  url "https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip"
+  # url "https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip"
+  url "https://github.com/tomokuni/Myrica/blob/a2383726d3662453ef7efa37862f743a831c7433/product/MyricaM.7z?raw=true"
   name 'MyricaM'
   homepage 'https://myrica.estable.jp/'
 
