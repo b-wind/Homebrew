31153b6999276c31ec5716b63ce2bc2be14c543e
b-wind <perl.mania@gmail.com>
2019-11-28 02:19:42 +0900

    add font-myrica/font-myricam casks

---
+++ Casks/font-myrica.rb
@@ -0,0 +1,10 @@
+cask 'font-myrica' do
+  version '2.012.20180119'
+  sha256 'ffab16c40a9610989874e9375859e00ae3c137a86dc34afa8ebedeaf212e1be9'
+
+  url "https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip"
+  name 'Myrica'
+  homepage 'https://myrica.estable.jp/'
+
+  font "Myrica.TTC"
+end
---
+++ Casks/font-myricam.rb
@@ -0,0 +1,10 @@
+cask 'font-myricam' do
+  version '2.012.20180119'
+  sha256 'd1bb313ba9d3d22c7a084369ef9463c75735d3165a223c66596cd60f285379a6'
+
+  url "https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip"
+  name 'MyricaM'
+  homepage 'https://myrica.estable.jp/'
+
+  font "MyricaM.TTC"
+end
