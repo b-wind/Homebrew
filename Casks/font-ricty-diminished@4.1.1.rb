4b024c5a2d21e23a5ef7d51c9e82cef769148f56
b-wind <perl.mania@gmail.com>
2019-11-28 03:34:47 +0900

    add version

--- Casks/font-ricty-diminished@4.1.1.rb
+++ Casks/font-ricty-diminished@4.1.1.rb
@@ -1,9 +1,9 @@
-cask 'font-ricty-diminished' do
+cask 'font-ricty-diminished@4.1.1' do
   version '4.1.1'
   sha256 '09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b'
 
   url "http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
-  name 'Ricty Diminished'
+  name 'Ricty Diminished@4.1.1'
   homepage 'http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished.html'
 
   font 'RictyDiminished-Bold.ttf'
