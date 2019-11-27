3e6bb19e2ff423715a54d999fd7032fd01bfb6ac
b-wind <perl.mania@gmail.com>
2019-11-28 03:16:44 +0900

    update name

--- Casks/font-myrica-at-2.012.20180119.rb
+++ Casks/font-myrica-at-2.012.20180119.rb
@@ -3,7 +3,7 @@ cask 'font-myrica' do
   sha256 'f73dca7a5999d02351534dbdc184e0e2f9f1a8294de5ff931bb535a5327c68c0'
 
   url "https://github.com/tomokuni/Myrica/blob/a2383726d3662453ef7efa37862f743a831c7433/product/Myrica.7z?raw=true"
-  name 'Myrica'
+  name 'Myrica@2.012.20180119'
   homepage 'https://myrica.estable.jp/'
 
   font "Myrica.TTC"
--- Casks/font-myricam-at-2.012.20180119.rb
+++ Casks/font-myricam-at-2.012.20180119.rb
@@ -3,7 +3,7 @@ cask 'font-myricam' do
   sha256 '7b309919d0f5e51cb2d96dc13b94794efe39f2b2f4eae7a43d9404088ad2ddda'
 
   url "https://github.com/tomokuni/Myrica/blob/a2383726d3662453ef7efa37862f743a831c7433/product/MyricaM.7z?raw=true"
-  name 'MyricaM'
+  name 'MyricaM@2.012.20180119'
   homepage 'https://myrica.estable.jp/'
 
   font "MyricaM.TTC"
